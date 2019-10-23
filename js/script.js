8f57fdc3f1baab9c3c6d651376ab2f0db7f57269
NAKNAO-nnct <totoro.windows@outlook.com>
2019-10-24 03:43:19 +0900

    第2版完成

--- js/script.js
+++ js/script.js
@@ -1,42 +1,51 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(
-    document.querySelectorAll(".navbar-burger"),
-    0
-  );
+// modle画面のOn/off
+function modal_open(map_num, place_num) {
+  let img = document.getElementById('p' + place_num);
+  img.children[0].setAttribute('fill', '#e3170a');
+  img.children[1].setAttribute('stroke', '#fdfffc');
+  img.classList.add('flag');
+  const modals = document.getElementById(map_num).classList.toggle("is-active");
+}
 
-  // Check if there are any navbar burgers
-  if ($navbarBurgers.length > 0) {
-    // Add a click event on each of them
-    $navbarBurgers.forEach(el => {
-      el.addEventListener("click", () => {
-        // Get the target from the "data-target" attribute
-        const target = el.dataset.target;
-        const $target = document.getElementById(target);
-
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        el.classList.toggle("is-active");
-        $target.classList.toggle("is-active");
-      });
-    });
+function modal_close() {
+  const modals = document.getElementsByClassName('modal');
+  for (let i = 0; i < modals.length; i++) {
+    modals[i].classList.remove("is-active");
   }
-});
+  let point_place = document.getElementsByClassName('flag')[0];
+  point_place.children[0].setAttribute('fill', '#FFFFFF');
+  point_place.children[1].setAttribute('stroke', '');
+  point_place.classList.remove('flag');
+}
 
 // 記事を読み込む
 fetch("json/shop.json")
-  .then(function(response) {
+  .then(function (response) {
     return response.json();
   })
-  .then(function(myJson) {
-    console.log(JSON.stringify(myJson));
+  .then(function (myJson) {
+    // console.log(JSON.stringify(myJson));
     initVue(myJson);
   });
 
 function initVue(data) {
   new Vue({
-    el: "#shop",
+    el: "#nav",
     data: {
-      groups: data["fes"]
+      showNav: false
     }
+  });
+  new Vue({
+    el: "#app",
+    data: {
+      groups: data["fes"],
+      dep: data["dep"],
+      plan: data["plan"]
+    },
+    methods: {
+      open: function (map, place) {
+        modal_open(map, place);
+      }
+    },
   });
 }
